909662694adb03c5f32d1a852ba1076532e8aee1
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-02-20 20:21:15 +0000

    fr

--- siade25/settings.py
+++ siade25/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
+        'HOST': config('DB_HOST', default='cadb'),
         'PORT': config('DB_PORT', default=5432, cast=int),
     }
 }
